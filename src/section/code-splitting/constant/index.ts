8a851b2ae6dbe6d1a26f7a2432bd13e8e6b7a1d0
irfan andriansyah <irfanandriansyah10@gmail.com>
2021-10-15 14:03:02 +0700

    feat(ui): create sample code for code splitting part 3

--- src/section/code-splitting/constant/index.ts
+++ src/section/code-splitting/constant/index.ts
@@ -9,6 +9,10 @@ export const SIDEBAR_MENU_CODE_SPLITTING_SECTION: ISidebarProps = {
     {
       text: `Eager Load Code Splitting`,
       to: `/code-splitting/part-2`
+    },
+    {
+      text: `Webpack Magic Comment`,
+      to: `/code-splitting/part-3`
     }
   ],
   subtitle: `Cheat Sheet Implement Code Splitting On React JS`,
