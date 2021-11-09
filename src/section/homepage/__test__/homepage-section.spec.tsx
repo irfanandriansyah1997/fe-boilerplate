9c3e5e2116dbb1d263f3e8ad1dc1988bb182ccac
irfan andriansyah <irfanandriansyah10@gmail.com>
2021-11-09 11:09:16 +0700

    test(section): create test case homepage section

---
+++ src/section/homepage/__test__/homepage-section.spec.tsx
@@ -0,0 +1,95 @@
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+
+import { IMasonryItem } from '../../../components/organisms/masonry-layout/interface';
+import Homepage from '..';
+
+jest.mock(`../helper/index`, () => ({
+  generateSampleLink: jest.fn().mockImplementation((): IMasonryItem[] => [
+    {
+      item: {
+        menu: [
+          {
+            text: `Wrap Use Memo`,
+            to: `/context/part-1`
+          },
+          {
+            text: `Separate Context Value`,
+            to: `/context/part-2`
+          },
+          {
+            text: `Colocate State`,
+            to: `/context/part-3`
+          }
+        ],
+        subtitle: `Sample Subtitle Course`,
+        title: `Title Course`
+      },
+      key: `key 1`
+    }
+  ])
+}));
+
+describe(`Testing Homepage Section`, () => {
+  it(`Snapshot Testing`, () => {
+    const { container } = render(
+      <MemoryRouter>
+        <Homepage />
+      </MemoryRouter>
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it(`Should be rendered title and description on homepage`, () => {
+    const { getByTestId } = render(
+      <MemoryRouter>
+        <Homepage />
+      </MemoryRouter>
+    );
+
+    expect(getByTestId(/homepage-title/)).toHaveTextContent(
+      `React.js cheatsheet`
+    );
+    expect(getByTestId(/homepage-description/)).toHaveTextContent(
+      `React is a JavaScript library for building user interfaces. This guide targets React v15 to v16.`
+    );
+  });
+
+  it(`Should be rendered one card on homepage`, () => {
+    const { getAllByTestId, queryAllByTestId, queryByText } = render(
+      <MemoryRouter>
+        <Homepage />
+      </MemoryRouter>
+    );
+
+    expect(getAllByTestId(/^homepage-card$/i)).toHaveLength(1);
+    expect(queryByText(/^Title Course$/i)).toBeInTheDocument();
+    expect(queryByText(/^Sample Subtitle Course$/i)).toBeInTheDocument();
+
+    expect(queryAllByTestId(/homepage-card-link/)).toHaveLength(3);
+    const [firstElement, secondElement, thirdElement] = queryAllByTestId(
+      /homepage-card-link/
+    );
+
+    /**
+     * Check Attribute First Item
+     */
+    expect(firstElement).toHaveTextContent(`sticky_note_2Wrap Use Memo`);
+    expect(firstElement).toHaveAttribute(`href`, `/context/part-1`);
+
+    /**
+     * Check Attribute Second Item
+     */
+    expect(secondElement).toHaveTextContent(
+      `sticky_note_2Separate Context Value`
+    );
+    expect(secondElement).toHaveAttribute(`href`, `/context/part-2`);
+
+    /**
+     * Check Attribute Third Item
+     */
+    expect(thirdElement).toHaveTextContent(`sticky_note_2Colocate State`);
+    expect(thirdElement).toHaveAttribute(`href`, `/context/part-3`);
+  });
+});
