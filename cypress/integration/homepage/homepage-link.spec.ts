7818d6d6ea2faeb7f9425d278d8027fa3187ce27
irfan andriansyah <irfanandriansyah10@gmail.com>
2021-11-11 08:43:47 +0700

    test(e2e): create sample test for assert link and redirect url

--- cypress/integration/homepage/homepage-link.spec.ts
+++ cypress/integration/homepage/homepage-link.spec.ts
@@ -46,9 +46,41 @@ describe(`Testing Homepage Link`, () => {
       cy.viewport(`macbook-15`).visit(Cypress.env(`HOME_PAGE_URL`));
     });
 
-    it(`Should redirect to \`use-memo/part-1\``);
-    it(`Should redirect to \`use-memo/part-2\``);
-    it(`Should redirect to \`use-memo/part-3\``);
+    it(`Should redirect to \`use-memo/part-1\``, () => {
+      cy.doClickHomepageCardLinkItem({
+        elementIndex: 1,
+        expectedURL: `${Cypress.env(`USE_MEMO_PAGE_URL`)}/part-1`,
+        linkSelector: /^Wrap Function in Use Memo$/i
+      });
+
+      cy.findByTestId(/use-memo-1/).should(`exist`);
+      cy.findByTestId(/use-memo-2/).should(`not.exist`);
+      cy.findByTestId(/use-memo-3/).should(`not.exist`);
+    });
+
+    it(`Should redirect to \`use-memo/part-2\``, () => {
+      cy.doClickHomepageCardLinkItem({
+        elementIndex: 1,
+        expectedURL: `${Cypress.env(`USE_MEMO_PAGE_URL`)}/part-2`,
+        linkSelector: /^Implement Web Worker$/i
+      });
+
+      cy.findByTestId(/use-memo-1/).should(`not.exist`);
+      cy.findByTestId(/use-memo-2/).should(`exist`);
+      cy.findByTestId(/use-memo-3/).should(`not.exist`);
+    });
+
+    it(`Should redirect to \`use-memo/part-3\``, () => {
+      cy.doClickHomepageCardLinkItem({
+        elementIndex: 1,
+        expectedURL: `${Cypress.env(`USE_MEMO_PAGE_URL`)}/part-3`,
+        linkSelector: /^Window Large List Render$/i
+      });
+
+      cy.findByTestId(/use-memo-1/).should(`not.exist`);
+      cy.findByTestId(/use-memo-2/).should(`not.exist`);
+      cy.findByTestId(/use-memo-3/).should(`exist`);
+    });
   });
 
   describe(`Assert Link On Optimize Context Value`, () => {
@@ -56,11 +88,94 @@ describe(`Testing Homepage Link`, () => {
       cy.viewport(`macbook-15`).visit(Cypress.env(`HOME_PAGE_URL`));
     });
 
-    it(`Should redirect to \`context/part-1\``);
-    it(`Should redirect to \`context/part-2\``);
-    it(`Should redirect to \`context/part-3\``);
-    it(`Should redirect to \`context/part-4\``);
-    it(`Should redirect to \`context/part-5\``);
-    it(`Should redirect to \`context/part-6\``);
+    it(`Should redirect to \`context/part-1\``, () => {
+      cy.doClickHomepageCardLinkItem({
+        elementIndex: 2,
+        expectedURL: `${Cypress.env(`CONTEXT_PAGE_URL`)}/part-1`,
+        linkSelector: /^Wrap Use Memo$/i
+      });
+
+      cy.findByTestId(/context-1/).should(`exist`);
+      cy.findByTestId(/context-2/).should(`not.exist`);
+      cy.findByTestId(/context-3/).should(`not.exist`);
+      cy.findByTestId(/context-4/).should(`not.exist`);
+      cy.findByTestId(/context-5/).should(`not.exist`);
+      cy.findByTestId(/context-6/).should(`not.exist`);
+    });
+
+    it(`Should redirect to \`context/part-2\``, () => {
+      cy.doClickHomepageCardLinkItem({
+        elementIndex: 2,
+        expectedURL: `${Cypress.env(`CONTEXT_PAGE_URL`)}/part-2`,
+        linkSelector: /^Separate Context Value$/i
+      });
+
+      cy.findByTestId(/context-1/).should(`not.exist`);
+      cy.findByTestId(/context-2/).should(`exist`);
+      cy.findByTestId(/context-3/).should(`not.exist`);
+      cy.findByTestId(/context-4/).should(`not.exist`);
+      cy.findByTestId(/context-5/).should(`not.exist`);
+      cy.findByTestId(/context-6/).should(`not.exist`);
+    });
+
+    it(`Should redirect to \`context/part-3\``, () => {
+      cy.doClickHomepageCardLinkItem({
+        elementIndex: 2,
+        expectedURL: `${Cypress.env(`CONTEXT_PAGE_URL`)}/part-3`,
+        linkSelector: /^Colocate State$/i
+      });
+
+      cy.findByTestId(/context-1/).should(`not.exist`);
+      cy.findByTestId(/context-2/).should(`not.exist`);
+      cy.findByTestId(/context-3/).should(`exist`);
+      cy.findByTestId(/context-4/).should(`not.exist`);
+      cy.findByTestId(/context-5/).should(`not.exist`);
+      cy.findByTestId(/context-6/).should(`not.exist`);
+    });
+
+    it(`Should redirect to \`context/part-4\``, () => {
+      cy.doClickHomepageCardLinkItem({
+        elementIndex: 2,
+        expectedURL: `${Cypress.env(`CONTEXT_PAGE_URL`)}/part-4`,
+        linkSelector: /^Separate Context Dog Name$/i
+      });
+
+      cy.findByTestId(/context-1/).should(`not.exist`);
+      cy.findByTestId(/context-2/).should(`not.exist`);
+      cy.findByTestId(/context-3/).should(`not.exist`);
+      cy.findByTestId(/context-4/).should(`exist`);
+      cy.findByTestId(/context-5/).should(`not.exist`);
+      cy.findByTestId(/context-6/).should(`not.exist`);
+    });
+
+    it(`Should redirect to \`context/part-5\``, () => {
+      cy.doClickHomepageCardLinkItem({
+        elementIndex: 2,
+        expectedURL: `${Cypress.env(`CONTEXT_PAGE_URL`)}/part-5`,
+        linkSelector: /^Memoized Cell Component$/i
+      });
+
+      cy.findByTestId(/context-1/).should(`not.exist`);
+      cy.findByTestId(/context-2/).should(`not.exist`);
+      cy.findByTestId(/context-3/).should(`not.exist`);
+      cy.findByTestId(/context-4/).should(`not.exist`);
+      cy.findByTestId(/context-5/).should(`exist`);
+      cy.findByTestId(/context-6/).should(`not.exist`);
+    });
+
+    it(`Should redirect to \`context/part-6\``, () => {
+      cy.doClickHomepageCardLinkItem({
+        elementIndex: 2,
+        expectedURL: `${Cypress.env(`CONTEXT_PAGE_URL`)}/part-6`,
+        linkSelector: /^Refactor To Recoil$/i
+      });
+
+      cy.findByTestId(/context-1/).should(`not.exist`);
+      cy.findByTestId(/context-2/).should(`not.exist`);
+      cy.findByTestId(/context-3/).should(`not.exist`);
+      cy.findByTestId(/context-4/).should(`not.exist`);
+      cy.findByTestId(/context-5/).should(`not.exist`);
+      cy.findByTestId(/context-6/).should(`exist`);
+    });
   });
 });
